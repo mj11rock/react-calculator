38a7141f6a2bfe198c8437a146ab3510ed8a317f
mj11rock <mj11rock@gmail.com>
2019-07-04 23:03:15 +0500

    Updates

---
+++ precache-manifest.07727a6dcb761f9f7c6b4593032d11bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b55cc2c1d168629565a52973dd5fe25",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "ef954a7980086373762d",
+    "url": "/react-calculator/static/css/main.49295da7.chunk.css"
+  },
+  {
+    "revision": "240f52b1a9a1e5ba49c4",
+    "url": "/react-calculator/static/js/2.ee645e81.chunk.js"
+  },
+  {
+    "revision": "ef954a7980086373762d",
+    "url": "/react-calculator/static/js/main.b3d09cd3.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  }
+]);
